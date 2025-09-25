e78ec07401d4daf5419d1cc66d13c5bd2146203a
Nabaeep <nabadeepdutta8@gmail.com>
2025-09-25 13:52:08 +0530

    final push

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ app.use(cors());
 app.get("/", async (req, res) => {
   try {
 const browser = await puppeteer.launch({
-  headless: true,
-  executablePath: '/usr/bin/chromium-browser', // or '/usr/bin/chromium'
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
-
-
 
 
 
